c885fcb518abf42c73b105b550f87c73178afa8f
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-21 08:12:05 -0700

    add object and property

--- tugas8.js
+++ tugas8.js
@@ -1,5 +1,11 @@
 function dataObject() {
-
+    var handphone = {
+        type: 'samsung',
+        chipset: 'snap dragon',
+        camera: '48 mp',
+        harga: 5000000,
+    }
+    console.log(handphone)
 }
 
 dataObject()
