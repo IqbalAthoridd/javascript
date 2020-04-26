c44e9dba3afb197c5ec4c4f4269761735c43e82a
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 03:45:08 -0700

    mengubah hurup lowercase dan uppercase

---
+++ tugas17a.js
@@ -0,0 +1,12 @@
+function ubahHuruf() {
+    var huruf1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+    var huruf2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+
+    console.log(huruf1.toLowerCase())
+    console.log(huruf1.toUpperCase())
+
+    console.log(huruf2.toLowerCase())
+    console.log(huruf2.toUpperCase())
+}
+
+ubahHuruf()
