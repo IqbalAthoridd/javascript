f3c0091a6475ed1520b6ca0badbf05538e2a9910
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 00:30:43 -0700

    function min,max

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+var angka = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10];
+
+var min = Math.min(...angka);
+var max = Math.max(...angka);
+
+console.log("Nilai Minimal :", min)
+console.log("Nilai Maksimal :", max)
