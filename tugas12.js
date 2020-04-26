eaeb175d4167603e8ed12b00abfeb8e029df596c
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-25 21:19:37 -0700

    add Pop and Push

--- tugas12.js
+++ tugas12.js
@@ -1,3 +1,11 @@
 function popShift() {
     var buah = ['pisang', 'jeruk', 'apel', 'mangga'];
+    console.log(buah)
+
+    buah.pop()
+    buah.push()
+
+    return buah
 }
+
+console.log(popShift())
