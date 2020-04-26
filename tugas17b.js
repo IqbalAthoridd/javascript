bb7e1e64909c1285f054ce02e1be41cd0505bc1f
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 03:46:07 -0700

    menampilkan code ASCII dari string

---
+++ tugas17b.js
@@ -0,0 +1,7 @@
+function codeAscii() {
+    var huruf = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+    console.log(huruf.charCodeAt(0), huruf.charCodeAt(1), huruf.charCodeAt(2), huruf.charCodeAt(3), huruf.charCodeAt(4), huruf.charCodeAt(5))
+}
+
+codeAscii()
