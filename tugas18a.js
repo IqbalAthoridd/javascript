3ca5917c9eb5aefda7e6c22dc7dda9e6113c1c86
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 19:50:45 -0700

    String slice

---
+++ tugas18a.js
@@ -0,0 +1,7 @@
+function sliceKata() {
+    var kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+
+    console.log(kata.slice(8, 25))
+}
+
+sliceKata()
