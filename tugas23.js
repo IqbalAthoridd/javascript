0b3119d302e0c85cb3510f752e82656294e69f7d
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-27 01:22:23 -0700

    array filter

---
+++ tugas23.js
@@ -0,0 +1,11 @@
+function arrayFilter() {
+    var data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+    var result = data.filter(data => {
+        return data > 15
+    })
+
+    return result
+}
+
+console.log(arrayFilter())
