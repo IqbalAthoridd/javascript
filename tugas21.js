19237b519e3397bd7dccbed82afc3dca34bd78df
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 23:08:48 -0700

    replace kata

---
+++ tugas21.js
@@ -0,0 +1,10 @@
+function ubahKata() {
+    var kalimat = "saya belajar masak nasi goreng";
+    console.log(kalimat)
+
+    var ganti = kalimat.replace("masak", "makan")
+
+    return ganti
+}
+
+console.log(ubahKata())
