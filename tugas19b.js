445d6b6f81d4ace025448b1ab73ac0e3a037b589
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-26 20:41:42 -0700

    string repeat

---
+++ tugas19b.js
@@ -0,0 +1,14 @@
+function repeatKalimat() {
+    var kalimat = "*";
+
+    console.log(kalimat.repeat(5))
+    console.log(kalimat.repeat(25))
+    console.log(kalimat.repeat(45))
+    console.log(kalimat.repeat(65))
+    console.log(kalimat.repeat(95))
+    console.log(kalimat.repeat(45))
+    console.log(kalimat.repeat(25))
+    console.log(kalimat.repeat(5))
+}
+
+repeatKalimat()
