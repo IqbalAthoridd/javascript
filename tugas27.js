29cf835b3c5ab1e057832722d19ec5cd885eac14
Iqbal Athorid <iqbaathoridd@gmail.com>
2020-04-27 21:12:59 -0700

    konversi ke string

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+var a = "No";
+var b = " Rumah Saya ";
+var c = 30;
+var d = "A";
+var e = "Jalan Pangeran Tirtayasa NO";
+var f = 70;
+
+var result = a + b + String(c) + d;
+var result2 = e + String(f)
+
+console.log(result)
+console.log(result2)
